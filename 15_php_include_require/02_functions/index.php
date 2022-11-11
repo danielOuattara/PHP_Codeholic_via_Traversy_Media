4dc940d38ec948ed623f56c86accd2af3123053e
danielOuattara <daniel.ouattara@gmx.com>
2022-11-11 23:48:47 +0100

    php include require

---
+++ 15_php_include_require/02_functions/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('./math.php');
+
+
+echo $add(4, 5), "<br/>";
+
+echo $substract(4, 5), "<br />";
+
+echo "Hello";
---
+++ 15_php_include_require/02_functions/math.php
@@ -0,0 +1,4 @@
+<?php
+
+$add = fn ($a, $b) => $a + $b;
+$substract = fn ($a, $b) => $a - $b;
