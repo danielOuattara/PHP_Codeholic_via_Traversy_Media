a941029b3d05d53b7198e97bd205c7747dd1b8d2
danielOuattara <daniel.ouattara@gmx.com>
2022-11-11 23:46:01 +0100

    php comments

---
+++ 06_php_comments/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php comments</title>
+</head>
+
+<body>
+
+    <?php
+    # comment line
+    // comment line
+
+    /* mutli-
+       lignes 
+       comments */
+
+
+    ?>
+    <?php echo "<p>Hello World !! </p>" ?>
+
+</body>
+
+</html>
